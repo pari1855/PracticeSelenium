8f094ebf27097ca8403a8126f1330c7a2766b3a1
VirendarrSinngh <VirendarrSinngh@Viren>
2020-03-18 18:01:02 +0530

    Commiting the project changes

---
+++ MiniHybridProject/src/main/java/Utility/BaseClass.java
@@ -0,0 +1,20 @@
+package Utility;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	public WebDriver driver;
+	public WebDriver driverInitialization() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		return driver;
+	}
+}
